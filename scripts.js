58857deb92d4b85773c60a143f73353a9333e81a
Siphesihle <Siphesihledyalvane@gmail.com>
2023-04-18 16:38:04 +0200

    called wlwmwnts using querySelector and assigned values

---
+++ scripts.js
@@ -0,0 +1,99 @@
+// script.js
+
+const root1 = document.querySelector('[data-key = "order1"]');
+
+const biscuits1 = root1.querySelector('[class = "biscuits"]');
+biscuits1.querySelector('[class="count"]').textContent = root1.dataset.biscuits;
+
+const donuts1 = root1.querySelector('[class = "donuts"]');
+donuts1.querySelector('[class ="count"]').textContent = root1.dataset.donuts;
+
+const pancakes1 = root1.querySelector('[class = "pancakes"]');
+pancakes1.querySelector('[class = "count"]').textContent = root1.dataset.pancakes;
+
+
+
+
+
+const root2 = document.querySelector('[data-key = "order2"]');
+
+const biscuits2 = root2.querySelector('[class = "biscuits"]');
+biscuits2.querySelector('[class ="count"]').textContent = root2.dataset.biscuits;
+
+const donuts2 = root2.querySelector('[class = "donuts"]');
+donuts2.querySelector('[class ="count"]').textContent = root2.dataset.donuts;
+
+const pancakes2 = root2.querySelector('[class = "pancakes"]');
+pancakes2.querySelector('[class = "count"]').textContent = root2.dataset.pancakes;
+
+
+
+const root3 = document.querySelector('[data-key = "order3"]');
+
+const biscuits3 = root3.querySelector('[class = "biscuits"]');
+biscuits3.querySelector('[class ="count"]').textContent = root3.dataset.biscuits;
+
+const donuts3 = root3.querySelector('[class = "donuts"]');
+donuts3.querySelector('[class ="count"]').textContent = root3.dataset.donuts;
+
+const pancakes3 = root3.querySelector('[class = "pancakes"]');
+pancakes3.querySelector('[class = "count"]').textContent = root3.dataset.pancakes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const 1-donuts: document(donuts),
+const 1-pancakes: document(pancakes),
+const 1-status: document(status)
+
+const 2-root = document(order2),
+const 2-biscuits: document(biscuits),
+const 2-donuts: document(donuts),
+const 2-pancakes: document(pancakes),
+const 2-status: document(status)
+
+const 3-root = document(order3),
+const 3-biscuits: document(biscuits),
+const 3-donuts: document(donuts),
+const 3-pancakes: document(pancakes),
+const 3-status: document(status)
+
+1-biscuits= 1-root.biscuits,
+1-donuts = 1-root.donuts,
+1-pancakes = 1-root.pancakes,
+1-status = 1-root.status ? Delivered : Pending
+
+2-biscuits= 2-root.biscuits,
+2-donuts = 2-root.donuts,
+2-pancakes = 2-root.pancakes,
+2-status = 2-root.status ? Delivered : Pending
+
+3-biscuits= 3-root.biscuits,
+3-donuts = 3-root.donuts,
+3-pancakes = 3-root.pancakes,
+3-status = 3-root.status ? Delivered : Pending*/
+//biscuits1.querySelector('[class="count"]').textContent = root1.dataset.biscuits;
+//donuts1.root1.querySelector('[class ="count"]').textContent = root1.dataset.donuts;
+
+
+
+
+
